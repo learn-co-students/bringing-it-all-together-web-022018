cd52f2c721c89e08df1f174f824678397f6e727e
smarks8 <staceyamarks@gmail.com>
2018-03-01 09:16:43 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Dog
   attr_accessor :name, :breed, :id
 
@@ -88,6 +89,7 @@ class Dog
       if !dog.empty?
         dog_data = dog[0]
         dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+        binding.pry
       else
         dog = self.create(name: name, breed: breed)
       end
